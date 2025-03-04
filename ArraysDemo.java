5cd1d0df5d9f861075c5e1d1c5498604c66d91a8
AarkayITsolutions <alihassanali119683@gmail.com>
2025-03-04 19:42:17 +0500

    adding files

---
+++ ArraysDemo.java
@@ -0,0 +1,20 @@
+// ArraysDemo.java
+// Topic: Understanding Arrays in Java
+
+/*
+ * Explanation:
+ * This program demonstrates the use of arrays by storing and displaying a set of integers.
+ */
+
+public class ArraysDemo {
+    public static void main(String[] args) {
+        // Declare and initialize an array
+        int[] numbers = {10, 20, 30, 40, 50};
+        
+        // Display array elements using a for loop
+        System.out.println("Array elements:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
