e408b5de516e69ec7c2a4ddd0631a311768900c3
AarkayITsolutions <alihassanali119683@gmail.com>
2025-03-04 19:42:32 +0500

    adding files

---
+++ ConditionalStatements.java
@@ -0,0 +1,34 @@
+// ConditionalStatements.java
+// Topic: Using Conditional Statements in Java
+
+/*
+ * Explanation:
+ * Demonstrates the use of if-else and switch statements for decision-making.
+ */
+
+ public class ConditionalStatements {
+    public static void main(String[] args) {
+        int number = 10;
+        
+        // if-else statement
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else {
+            System.out.println("The number is not positive.");
+        }
+        
+        // switch statement
+        int day = 2;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            default:
+                System.out.println("Other day");
+                break;
+        }
+    }
+}
