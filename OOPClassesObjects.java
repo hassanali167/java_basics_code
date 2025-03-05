a5b4825af1ba4a5769a1bc38cdcccfd097b1bc3e
AarkayITsolutions <alihassanali119683@gmail.com>
2025-03-05 12:49:20 +0500

    adding files

---
+++ OOPClassesObjects.java
@@ -0,0 +1,29 @@
+// OOPClassesObjects.java
+// Topic: Classes and Objects in Java
+
+/*
+ * Explanation:
+ * This program defines a class `Car` and creates objects of it to demonstrate object-oriented programming.
+ */
+
+class Car {
+    String brand;
+    
+    // Constructor
+    Car(String brand) {
+        this.brand = brand;
+    }
+    
+    // Method to display car brand
+    void display() {
+        System.out.println("Car brand: " + brand);
+    }
+}
+
+public class OOPClassesObjects {
+    public static void main(String[] args) {
+        // Creating objects of Car class
+        Car car1 = new Car("Toyota");
+        car1.display();
+    }
+}
