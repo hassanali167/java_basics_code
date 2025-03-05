55cacaf015dbb0c5e42f0bddbd384ebcd9579916
AarkayITsolutions <alihassanali119683@gmail.com>
2025-03-05 12:49:04 +0500

    adding files

---
+++ LambdaExpressionDemo.java
@@ -0,0 +1,23 @@
+// LambdaExpressionDemo.java
+// Topic: Using Lambda Expressions in Java
+
+/*
+ * Explanation:
+ * This program demonstrates lambda expressions for functional programming.
+ */
+
+import java.util.ArrayList;
+
+public class LambdaExpressionDemo {
+    public static void main(String[] args) {
+        // Using a lambda expression to print a list of numbers
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+        numbers.add(40);
+        
+        System.out.println("Using Lambda Expression:");
+        numbers.forEach(n -> System.out.println(n));
+    }
+}
