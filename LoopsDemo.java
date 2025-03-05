d4e4c2081f9385e4e7b27423b96593301f27ccc0
AarkayITsolutions <alihassanali119683@gmail.com>
2025-03-05 12:49:11 +0500

    adding files

---
+++ LoopsDemo.java
@@ -0,0 +1,33 @@
+// LoopsDemo.java
+// Topic: Demonstrating Loops in Java
+
+/*
+ * Explanation:
+ * This program demonstrates the usage of for, while, and do-while loops in Java.
+ */
+
+public class LoopsDemo {
+    public static void main(String[] args) {
+        // For loop: Prints numbers from 1 to 5
+        System.out.println("For loop:");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+
+        // While loop: Prints numbers from 1 to 5
+        System.out.println("While loop:");
+        int j = 1;
+        while (j <= 5) {
+            System.out.println(j);
+            j++;
+        }
+
+        // Do-while loop: Prints numbers from 1 to 5
+        System.out.println("Do-While loop:");
+        int k = 1;
+        do {
+            System.out.println(k);
+            k++;
+        } while (k <= 5);
+    }
+}
