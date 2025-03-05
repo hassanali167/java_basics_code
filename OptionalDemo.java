bde4d7f39a43920bc7f750f5f1760adfe5089cec
AarkayITsolutions <alihassanali119683@gmail.com>
2025-03-05 12:50:58 +0500

    adding files

---
+++ OptionalDemo.java
@@ -0,0 +1,23 @@
+// OptionalDemo.java
+// Topic: Using Optional in Java to Avoid NullPointerException
+
+/*
+ * Explanation:
+ * This program demonstrates how to use the Optional class to handle null values safely.
+ */
+
+import java.util.Optional;
+
+public class OptionalDemo {
+    public static void main(String[] args) {
+        // Creating an Optional object
+        Optional<String> optionalValue = Optional.ofNullable(null);
+
+        // Checking if value is present and providing a default
+        String result = optionalValue.orElse("Default Value");
+        System.out.println("Result: " + result);
+
+        // Using Optional with lambda
+        optionalValue.ifPresent(value -> System.out.println("Value: " + value));
+    }
+}
